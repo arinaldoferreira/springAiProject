6ec61d322017b6ba646b45f9110e690421f1f17a
Arinaldo Ferreira <arinaldo.ferreira@outlook.com>
2026-06-21 09:01:53 -0300

    Criação Projeto api-voice

---
+++ api-voice/src/main/java/br/com/erudio/Startup2.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup2 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup2.class, args);
+	}
+
+}
